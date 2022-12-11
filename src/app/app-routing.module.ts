828f3708c8a0aaf240c19a052d80d9f2ccb2e7df
Kaoschuks <kaoschuks@hotmail.com>
2022-12-11 15:19:19 +0100

    added core design and removed template files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', loadChildren: () => import('./pages/pages.module').then( m => m.PagesModule), component: BaseLayoutComponent, 
+  },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConvertProvider } from './providers/convert-provider/convert-provider.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interview_test';
+
+  constructor(
+    private convert: ConvertProvider
+  ) {
+    this.convert.getCurrency()
+  }
 }
