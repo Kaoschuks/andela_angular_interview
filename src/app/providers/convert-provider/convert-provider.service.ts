eab13ddc853d79a1036d4db0156ebfc8f79d73a8
Kaoschuks <kaoschuks@hotmail.com>
2022-12-11 16:57:33 +0100

    ADDE CCOMMENTS

--- src/app/providers/convert-provider/convert-provider.service.ts
+++ src/app/providers/convert-provider/convert-provider.service.ts
@@ -15,12 +15,16 @@ export class ConvertProvider{
   histories: Array<ICurrencyModel> = []
   constructor() { }
 
+
+  // convert from currency to to ccurrnenccy (USD - EUR) BY EXCHANGE RATE
   convertCurrency(amount: number, from_currency: string, to_currency: string) {
     this.to_currency = to_currency;
     this.from_currency = from_currency;
     return amount * this.rates[to_currency];
   }
 
+
+  // get current exchannge rate
   getCurrency() {
     $.get('https://openexchangerates.org/api/latest.json', {app_id: environment.exchange.appid}, (data: any) => {
       this.rates = data.rates
@@ -29,11 +33,15 @@ export class ConvertProvider{
     })
   }
 
+
+  // save currency cocnvversion history
   saveHistory(history: ICurrencyModel) {
     this.histories.push(history)
     localStorage.setItem('history', JSON.stringify(this.histories));
   }
 
+
+  // get currency cocnvversion history
   getHistory() {
     this.histories = JSON.parse(localStorage.getItem('history') || '');
     return this.histories
