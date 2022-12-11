c6c3aacab0c0786318ef56fa57e031048a059d59
Kaoschuks <kaoschuks@hotmail.com>
2022-12-11 15:26:02 +0100

    added page router

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsPageComponent } from './details-page/details-page.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'overview',
-    pathMatch: 'full'
-  },
+  { path: '', component: OverviewPageComponent  },
+  { path: ':currency', component: DetailsPageComponent  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppsRoutingModule { }
+export class PagesRoutingModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,17 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppsRoutingModule } from './pages-routing.module';
+import { PagesRoutingModule } from './pages-routing.module';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
+
 
 @NgModule({
   declarations: [
+    OverviewPageComponent, DetailsPageComponent
   ],
   imports: [
     CommonModule, ComponentsModule,
-    AppsRoutingModule, 
+    PagesRoutingModule, 
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 
 })
-export class AppsModule { }
+export class PagesModule { }
